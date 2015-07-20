ed41b396b907fd8e25559b7bfa293f25ff040701
Peter.Li <yeshuanova@gmail.com>
2015-07-20 13:58:13 +0800

    Initial Commit

---
+++ OpenCLImageConvertor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCLImageConvertor
+//
+//  Created by Peter.Li on 2015/7/20.
+//  Copyright (c) 2015年 Peter.Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
